fafaf502541787e0d4ef5842373dbdfe96fd09e8
deepika-tj <deepika.anchan@techjini.com>
2018-06-01 12:27:30 +0530

    views edited

--- src/MerchantBundle/Form/MerchantType.php
+++ src/MerchantBundle/Form/MerchantType.php
@@ -9,6 +9,10 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class MerchantType extends AbstractType
 {
@@ -19,21 +23,23 @@ class MerchantType extends AbstractType
             ->add('contactPersonName', TextType::class, array('label' => 'Contact Person Name:'))
             ->add('address_line1', TextType::class, array('label' => 'Address_line1:'))
             ->add('address_line2', TextType::class, array('label' => 'Address_line2:'))
-            ->add('email', EmailType::class, array('label' => 'Email Id:'))
-            ->add('mobile_no', TextType::class, array('label' => 'Mobile Number:'))
+            ->add('email', EmailType::class, array('label' => 'Email Id:','required'=>''))
+            ->add('mobileNo', TelType::class, array('label' => 'Mobile Number:',
+                'required'=>true,'constraints'=>array(new Length(array('min'=>10, 'max'=>12)))
+            ))
             ->add('password', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'first_options' => array('label' => 'Password:'),
-                'second_options' => array('label' => 'Confirm Password:'),
+                'second_options' => array('label' => 'Confirm Password:','required'=>''),
             ))
             ->add('state', EntityType::class, array('class' => 'Common\Model\State',
                 'choice_label' => function ($state) {
                 return $state->getStateName();
-                }))
+                },'placeholder' => 'Choose a state'))
             ->add('country', EntityType::class, array('class' => 'Common\Model\Country',
                     'choice_label' => function ($country) {
                     return $country->getCountryName();
-                    }))
+                    },'placeholder' => 'Choose a country'))
             ->add('pincode',TextType::class, array('label' => 'Pincode:'))
             ->add('companylogo', FileType::class, array('label' => 'Company Logo'));
             
