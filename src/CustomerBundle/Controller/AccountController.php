36c10ae53a988e5a87c4c713708a28db35cc26cf
aishwaryamk96 <aishwarya.mk@techjini.com>
2018-06-11 15:59:16 +0530

    modifications

--- src/CustomerBundle/Controller/AccountController.php
+++ src/CustomerBundle/Controller/AccountController.php
@@ -122,6 +122,53 @@ class AccountController extends Controller
                         $image = $form->getData()["profile_photo"];
                         if($image)
                         {
+                            $bucketName = 'jiniproductphotos';
+                            $IAM_KEY = 'AKIAIYFN4B77GBAZAUMA';
+                             $IAM_SECRET = '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';
+
+                            if ($request->isMethod('POST')){
+                            try {
+       // You may need to change the region. It will say in the URL when the bucket is open                     // and on creation.
+                             $s3 = S3Client::factory(
+                              array(
+                                'credentials' => array(
+                                    'key' => $IAM_KEY,
+                                    'secret' => $IAM_SECRET
+                                ),
+                                'version' => 'latest',
+                                'region'  => 'us-east-1'
+                            )
+                        );
+                    } catch (Exception $e) {
+     // We use a die, so if this fails. It stops here. Typically this is a REST call so this would                        // return a json object.
+                        die("Error: " . $e->getMessage());
+                    }
+
+
+         // For this, I would generate a unqiue random string for the key name. But you can do whatever.
+                $keyName =  basename($_FILES["image"]['name']);
+                $pathInS3 = 'https://s3.console.aws.amazon.com/s3/buckets/jiniproductphotos/?region=us-east-1' . $keyName;
+                
+                    // Add it to S3
+                    try {
+                        // Uploaded:
+                        $file = $_FILES["image"]['tmp_name'];
+                        $s3->putObject(
+                            array(
+                                'Bucket'=>$bucketName,
+                                'Key' =>  $keyName,
+                                'SourceFile' => $file,
+                                'StorageClass' => 'REDUCED_REDUNDANCY',
+                                'ACL'    => 'public-read'
+                            )
+                        );
+                    } catch (S3Exception $e) {
+                        die('Error:' . $e->getMessage());
+                    } catch (Exception $e) {
+                        die('Error:' . $e->getMessage());
+                    }
+            } 
+                            
                         $imageName = $customer->getFname() . $customer->getLname() . '.' . $image->guessExtension();     
                         $image->move($this->getParameter('image_directory'),$imageName);
                         $customer->setProfilePhoto($imageName);
